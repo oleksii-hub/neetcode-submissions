4adb8da46964315a10c8353e6e8dd4bf69644451
Oleksii Zadorozhnyi <44027186+oleksii-hub@users.noreply.github.com>
2026-08-04 15:35:37 +0200

    Add: three-integer-sum - submission-26

---
+++ Data Structures & Algorithms/three-integer-sum/submission-26.cpp
@@ -0,0 +1,50 @@
+class Solution {
+public:
+    vector<vector<int>> threeSum(vector<int>& nums) {
+        if (nums.empty())
+            return {};
+
+        std::ranges::sort(nums);
+
+        std::vector<std::vector<int>> result;
+        std::ptrdiff_t n = std::ssize(nums);
+        for (std::ptrdiff_t i = 0; i < n - 2; ++i)
+        {
+            if (nums[i] > 0)
+                break;
+
+            if (i > 0 && nums[i] == nums[i - 1])
+                continue;
+
+            std::ptrdiff_t left = i + 1;
+            std::ptrdiff_t right = n - 1;
+
+            while (left < right)
+            {
+                int sum = nums[left] + nums[right];
+                if (sum + nums[i] == 0)
+                {
+                    result.push_back({nums[i], nums[left], nums[right]});
+
+                    --right;
+                    ++left;
+
+                    while (left < right && nums[left] == nums[left - 1])
+                        ++left;
+
+                    continue;
+                }
+
+                if (sum + nums[i] < 0)
+                {
+                    ++left;
+                    continue;
+                }
+
+                --right;
+            }
+        }
+
+        return std::vector(result.begin(), result.end());
+    }
+};
