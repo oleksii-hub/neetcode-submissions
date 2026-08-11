445a13e41f3a2cb7921b9c8025dcfdea8bb81332
Oleksii Zadorozhnyi <44027186+oleksii-hub@users.noreply.github.com>
2026-08-11 13:13:05 +0200

    Add: sliding-window-maximum - submission-2

---
+++ Data Structures & Algorithms/sliding-window-maximum/submission-2.cpp
@@ -0,0 +1,26 @@
+class Solution {
+public:
+    vector<int> maxSlidingWindow(vector<int>& nums, int k) {
+        // 6, 4, 2, 3, 5, 0, 1, ...
+        // 0, 1, 2, 3, 4, 5, 6, ...
+        const auto n = std::ssize(nums);
+        std::deque<std::ptrdiff_t> st;
+        std::vector<int> res;
+        res.reserve(n - k + 1);
+        for (std::ptrdiff_t r = 0; r < n; ++r)
+        {
+            while (!st.empty() && nums[r] > nums[st.back()])
+                st.pop_back();
+
+            st.push_back(r);
+
+            if (st.front() < r - k + 1)
+                st.pop_front();
+
+            if (r >= k - 1)
+                res.push_back(nums[st.front()]);
+        }
+        
+        return res;
+    }
+};
